c581d888cb9ba788a258d2ecad67a7f58c05d228
Murad Shahbazov <shahbazov.msh@gmail.com>
2023-12-07 19:27:24 +0100

    small fix

--- src/lib/validations/auth.ts
+++ src/lib/validations/auth.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-  username: z.string().trim().min(1, 'This field is required'),
-  password: z.string().trim().min(1, 'This field is required'),
+  username: z.string().trim().min(1, 'Username is required'),
+  password: z.string().trim().min(1, 'Password is required'),
 });
 
 export type LoginSchemaType = z.infer<typeof loginSchema>;
